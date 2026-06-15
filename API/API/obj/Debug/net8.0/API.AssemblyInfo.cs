a34b60d08fa4c9a78e479d128ab636176f2f490c
DanhNguyeen <Danhn9472@gmail.com>
2026-06-15 15:43:53 +0700

    Thêm Gitignore

--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e615be7cb53a558041c526988f9d0c013a14287e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6491814f8bf2ed355a24aeb6612f2b14b642e46")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
